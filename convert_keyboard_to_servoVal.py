f22fb87b5d1d7c2d18e73e4f4c0bb45c3d0aee99
Sleeplessgg <161692320+Sleeplessgg@users.noreply.github.com>
2024-10-16 12:11:31 +0000

    changed File Name and Rewrote whole Code to do what it should

--- convert_keyboard_to_servoVal.py
+++ convert_keyboard_to_servoVal.py
@@ -1,24 +1,22 @@
-from gpiozero import Servo                          # For Servo
 from pynput.keyboard import Key, Listener           # For Catching Keyboard Input
-from time import sleep                              # Delay
-
-servo = Servo(25)                                  # I/O Portnum on Raspberry Pi
+import serial                        # import Serial for Communication
+import time                          # Delay
 
 gotKeyPress = False
-servoAngle = 0
+
 
 def getInput(key):
     global gotKeyPress, servoAngle       # Set gotKeypress to False at the start
     
     if key.char == ('a'):
-        servoAngle = 0.5
+        servoAngle = 0
         gotKeyPress = True                          # Set gotKeypress to True if you get input 'a' or 'd'
-        print("a")
+
         
     if key.char == ('d'):
-        servoAngle = -0.5
+        servoAngle = 90
         gotKeyPress = True                          # Set gotKeypress to True if you get input 'a' or 'd'
-        print("d ")
+
         
     if key.char == ('w'):
         print("w")
@@ -37,12 +35,16 @@ with Listener(on_press = getInput) as listener:
     listener.join()
     
 try:
+    # create Serial Communication with Arduino
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
     while True:
         if gotKeyPress:
-            servo.value = servoAngle                    # Set Servo to Value determined by Keypress
+            ser.write(str(servoAngle).encode('utf-8'))  # Send ServoAngle
             sleep(0.1)
         else:
-            servo.value = 0                             # Set Servo to Nil if there was no Keypress
+            ser.write(str(45).encode('utf-8'))  # Send ServoAngle 45
             sleep(0.1)
         
         
